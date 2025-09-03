ab9b021488fc545ad73e5ca93155441920e458ec
Bojan Djordjevic <bojandjordjevic971@gmail.com>
2025-09-03 19:28:25 +0200

    predict_category.py is tested with test data

--- predict_category.py
+++ predict_category.py
@@ -8,12 +8,23 @@ print("Model loaded successfully!")
 print("Type 'exit' at any point to stop.\n")
 
 while True:
-    title = input(" Enter review title: ")
+    title = input(" Enter product title: ")
     if title.lower() == "exit":
         print("Exiting...")
         break
 
-    text = input(" Enter review text: ")
-    if text.lower() == "exit":
-        print("Exiting...")
-        break
+
+    # Compute title and word length:
+    title_length = len(title)
+    word_count = len(title.split())
+
+    # Create a DataFrame from input:
+    user_input = pd.DataFrame([{
+        "Product Title": title,
+        "title_length": title_length,
+        "word_count": word_count
+        }])
+
+    # Predict category:
+    prediction = model.predict(user_input)[0]
+    print(f" Predicted category: {prediction}\n" + "-" * 40)
